34623c5e490a05853d61f7d19109ec004013a115
Ariño Miguel (Contractor) <miguel.arin@umain.com>
2025-02-07 20:36:38 +0100

    Create mockGitLabData.js
    Mock json added

---
+++ src/mockGitLabData.js
@@ -0,0 +1,398 @@
+export const mockGitLabData = {
+    projects: [
+      {
+        id: 1,
+        name: "awesome-project",
+        description: "An awesome project with lots of features",
+        default_branch: "main",
+        ssh_url_to_repo: "git@gitlab.com:namespace/awesome-project.git",
+        http_url_to_repo: "https://gitlab.com/namespace/awesome-project.git",
+        web_url: "https://gitlab.com/namespace/awesome-project",
+        readme_url: "https://gitlab.com/namespace/awesome-project/-/blob/main/README.md",
+        avatar_url: "https://gitlab.com/uploads/-/system/project/avatar/1/project_avatar.png",
+        star_count: 150,
+        forks_count: 35,
+        last_activity_at: "2023-06-15T10:30:00Z",
+        namespace: { id: 10, name: "Namespace", path: "namespace" },
+        statistics: {
+          commit_count: 523,
+          storage_size: 1024000,
+          repository_size: 956000,
+          lfs_objects_size: 68000,
+          job_artifacts_size: 0
+        },
+        languages: {
+          "JavaScript": 60,
+          "Python": 25,
+          "HTML": 10,
+          "CSS": 5
+        }
+      },
+      {
+        id: 2,
+        name: "cool-library",
+        description: "A cool library for doing cool things",
+        default_branch: "master",
+        ssh_url_to_repo: "git@gitlab.com:namespace/cool-library.git",
+        http_url_to_repo: "https://gitlab.com/namespace/cool-library.git",
+        web_url: "https://gitlab.com/namespace/cool-library",
+        readme_url: "https://gitlab.com/namespace/cool-library/-/blob/master/README.md",
+        avatar_url: null,
+        star_count: 75,
+        forks_count: 20,
+        last_activity_at: "2023-06-14T15:45:00Z",
+        namespace: { id: 10, name: "Namespace", path: "namespace" },
+        statistics: {
+          commit_count: 231,
+          storage_size: 512000,
+          repository_size: 498000,
+          lfs_objects_size: 14000,
+          job_artifacts_size: 0
+        },
+        languages: {
+          "TypeScript": 80,
+          "JavaScript": 15,
+          "CSS": 5
+        }
+      },
+      {
+        id: 3,
+        name: "awesome-project",
+        description: "An awesome project with lots of features",
+        default_branch: "main",
+        ssh_url_to_repo: "git@gitlab.com:namespace/awesome-project.git",
+        http_url_to_repo: "https://gitlab.com/namespace/awesome-project.git",
+        web_url: "https://gitlab.com/namespace/awesome-project",
+        readme_url: "https://gitlab.com/namespace/awesome-project/-/blob/main/README.md",
+        avatar_url: "https://gitlab.com/uploads/-/system/project/avatar/1/project_avatar.png",
+        star_count: 150,
+        forks_count: 35,
+        last_activity_at: "2023-06-15T10:30:00Z",
+        namespace: { id: 10, name: "Namespace", path: "namespace" },
+        statistics: {
+          commit_count: 523,
+          storage_size: 1024000,
+          repository_size: 956000,
+          lfs_objects_size: 68000,
+          job_artifacts_size: 0
+        },
+        languages: {
+          "JavaScript": 60,
+          "Python": 25,
+          "HTML": 10,
+          "CSS": 5
+        }
+      },
+      {
+        id: 4,
+        name: "cool-library",
+        description: "A cool library for doing cool things",
+        default_branch: "master",
+        ssh_url_to_repo: "git@gitlab.com:namespace/cool-library.git",
+        http_url_to_repo: "https://gitlab.com/namespace/cool-library.git",
+        web_url: "https://gitlab.com/namespace/cool-library",
+        readme_url: "https://gitlab.com/namespace/cool-library/-/blob/master/README.md",
+        avatar_url: null,
+        star_count: 75,
+        forks_count: 20,
+        last_activity_at: "2023-06-14T15:45:00Z",
+        namespace: { id: 10, name: "Namespace", path: "namespace" },
+        statistics: {
+          commit_count: 231,
+          storage_size: 512000,
+          repository_size: 498000,
+          lfs_objects_size: 14000,
+          job_artifacts_size: 0
+        },
+        languages: {
+          "TypeScript": 80,
+          "JavaScript": 15,
+          "CSS": 5
+        }
+      },{
+        id: 5,
+        name: "awesome-project",
+        description: "An awesome project with lots of features",
+        default_branch: "main",
+        ssh_url_to_repo: "git@gitlab.com:namespace/awesome-project.git",
+        http_url_to_repo: "https://gitlab.com/namespace/awesome-project.git",
+        web_url: "https://gitlab.com/namespace/awesome-project",
+        readme_url: "https://gitlab.com/namespace/awesome-project/-/blob/main/README.md",
+        avatar_url: "https://gitlab.com/uploads/-/system/project/avatar/1/project_avatar.png",
+        star_count: 150,
+        forks_count: 35,
+        last_activity_at: "2023-06-15T10:30:00Z",
+        namespace: { id: 10, name: "Namespace", path: "namespace" },
+        statistics: {
+          commit_count: 523,
+          storage_size: 1024000,
+          repository_size: 956000,
+          lfs_objects_size: 68000,
+          job_artifacts_size: 0
+        },
+        languages: {
+          "JavaScript": 60,
+          "Python": 25,
+          "HTML": 10,
+          "CSS": 5
+        }
+      },
+      {
+        id: 6,
+        name: "cool-library",
+        description: "A cool library for doing cool things",
+        default_branch: "master",
+        ssh_url_to_repo: "git@gitlab.com:namespace/cool-library.git",
+        http_url_to_repo: "https://gitlab.com/namespace/cool-library.git",
+        web_url: "https://gitlab.com/namespace/cool-library",
+        readme_url: "https://gitlab.com/namespace/cool-library/-/blob/master/README.md",
+        avatar_url: null,
+        star_count: 75,
+        forks_count: 20,
+        last_activity_at: "2023-06-14T15:45:00Z",
+        namespace: { id: 10, name: "Namespace", path: "namespace" },
+        statistics: {
+          commit_count: 231,
+          storage_size: 512000,
+          repository_size: 498000,
+          lfs_objects_size: 14000,
+          job_artifacts_size: 0
+        },
+        languages: {
+          "TypeScript": 80,
+          "JavaScript": 15,
+          "CSS": 5
+        }
+      },{
+        id: 7,
+        name: "awesome-project",
+        description: "An awesome project with lots of features",
+        default_branch: "main",
+        ssh_url_to_repo: "git@gitlab.com:namespace/awesome-project.git",
+        http_url_to_repo: "https://gitlab.com/namespace/awesome-project.git",
+        web_url: "https://gitlab.com/namespace/awesome-project",
+        readme_url: "https://gitlab.com/namespace/awesome-project/-/blob/main/README.md",
+        avatar_url: "https://gitlab.com/uploads/-/system/project/avatar/1/project_avatar.png",
+        star_count: 150,
+        forks_count: 35,
+        last_activity_at: "2023-06-15T10:30:00Z",
+        namespace: { id: 10, name: "Namespace", path: "namespace" },
+        statistics: {
+          commit_count: 523,
+          storage_size: 1024000,
+          repository_size: 956000,
+          lfs_objects_size: 68000,
+          job_artifacts_size: 0
+        },
+        languages: {
+          "JavaScript": 60,
+          "Python": 25,
+          "HTML": 10,
+          "CSS": 5
+        }
+      },
+      {
+        id: 8,
+        name: "cool-library",
+        description: "A cool library for doing cool things",
+        default_branch: "master",
+        ssh_url_to_repo: "git@gitlab.com:namespace/cool-library.git",
+        http_url_to_repo: "https://gitlab.com/namespace/cool-library.git",
+        web_url: "https://gitlab.com/namespace/cool-library",
+        readme_url: "https://gitlab.com/namespace/cool-library/-/blob/master/README.md",
+        avatar_url: null,
+        star_count: 75,
+        forks_count: 20,
+        last_activity_at: "2023-06-14T15:45:00Z",
+        namespace: { id: 10, name: "Namespace", path: "namespace" },
+        statistics: {
+          commit_count: 231,
+          storage_size: 512000,
+          repository_size: 498000,
+          lfs_objects_size: 14000,
+          job_artifacts_size: 0
+        },
+        languages: {
+          "TypeScript": 80,
+          "JavaScript": 15,
+          "CSS": 5
+        }
+      },{
+        id: 9,
+        name: "awesome-project",
+        description: "An awesome project with lots of features",
+        default_branch: "main",
+        ssh_url_to_repo: "git@gitlab.com:namespace/awesome-project.git",
+        http_url_to_repo: "https://gitlab.com/namespace/awesome-project.git",
+        web_url: "https://gitlab.com/namespace/awesome-project",
+        readme_url: "https://gitlab.com/namespace/awesome-project/-/blob/main/README.md",
+        avatar_url: "https://gitlab.com/uploads/-/system/project/avatar/1/project_avatar.png",
+        star_count: 150,
+        forks_count: 35,
+        last_activity_at: "2023-06-15T10:30:00Z",
+        namespace: { id: 10, name: "Namespace", path: "namespace" },
+        statistics: {
+          commit_count: 523,
+          storage_size: 1024000,
+          repository_size: 956000,
+          lfs_objects_size: 68000,
+          job_artifacts_size: 0
+        },
+        languages: {
+          "JavaScript": 60,
+          "Python": 25,
+          "HTML": 10,
+          "CSS": 5
+        }
+      },
+      {
+        id: 10,
+        name: "cool-library",
+        description: "A cool library for doing cool things",
+        default_branch: "master",
+        ssh_url_to_repo: "git@gitlab.com:namespace/cool-library.git",
+        http_url_to_repo: "https://gitlab.com/namespace/cool-library.git",
+        web_url: "https://gitlab.com/namespace/cool-library",
+        readme_url: "https://gitlab.com/namespace/cool-library/-/blob/master/README.md",
+        avatar_url: null,
+        star_count: 75,
+        forks_count: 20,
+        last_activity_at: "2023-06-14T15:45:00Z",
+        namespace: { id: 10, name: "Namespace", path: "namespace" },
+        statistics: {
+          commit_count: 231,
+          storage_size: 512000,
+          repository_size: 498000,
+          lfs_objects_size: 14000,
+          job_artifacts_size: 0
+        },
+        languages: {
+          "TypeScript": 80,
+          "JavaScript": 15,
+          "CSS": 5
+        }
+      }
+    ],
+    users: [
+      {
+        id: 1,
+        username: "john_doe",
+        name: "John Doe",
+        state: "active",
+        avatar_url: "https://gitlab.com/uploads/-/system/user/avatar/1/avatar.png",
+        web_url: "https://gitlab.com/john_doe"
+      },
+      {
+        id: 2,
+        username: "jane_smith",
+        name: "Jane Smith",
+        state: "active",
+        avatar_url: "https://gitlab.com/uploads/-/system/user/avatar/2/avatar.png",
+        web_url: "https://gitlab.com/jane_smith"
+      }
+    ],
+    issues: [
+      {
+        id: 1,
+        iid: 1,
+        project_id: 1,
+        title: "Bug in login functionality",
+        description: "Users are unable to log in using their email addresses",
+        state: "opened",
+        created_at: "2023-06-10T09:00:00Z",
+        updated_at: "2023-06-15T11:00:00Z",
+        closed_at: null,
+        labels: ["bug", "critical"],
+        milestone: {
+          id: 1,
+          iid: 1,
+          project_id: 1,
+          title: "v1.0",
+          description: "Version 1.0 release",
+          due_date: "2023-07-01",
+          state: "active"
+        },
+        assignees: [
+          { id: 1, name: "John Doe", username: "john_doe" }
+        ],
+        author: { id: 2, name: "Jane Smith", username: "jane_smith" },
+        user_notes_count: 5,
+        upvotes: 2,
+        downvotes: 0
+      }
+    ],
+    merge_requests: [
+      {
+        id: 1,
+        iid: 1,
+        project_id: 1,
+        title: "Implement new feature XYZ",
+        description: "This MR implements the new XYZ feature as discussed in issue #10",
+        state: "opened",
+        created_at: "2023-06-12T14:00:00Z",
+        updated_at: "2023-06-15T16:00:00Z",
+        merged_at: null,
+        closed_at: null,
+        source_branch: "feature/xyz",
+        target_branch: "main",
+        user_notes_count: 3,
+        upvotes: 1,
+        downvotes: 0,
+        author: { id: 1, name: "John Doe", username: "john_doe" },
+        assignees: [
+          { id: 2, name: "Jane Smith", username: "jane_smith" }
+        ],
+        reviewers: [
+          { id: 2, name: "Jane Smith", username: "jane_smith" }
+        ],
+        source_project_id: 1,
+        target_project_id: 1,
+        labels: ["feature", "in-review"],
+        milestone: {
+          id: 1,
+          iid: 1,
+          project_id: 1,
+          title: "v1.0",
+          description: "Version 1.0 release",
+          due_date: "2023-07-01",
+          state: "active"
+        },
+        merge_status: "can_be_merged"
+      }
+    ],
+    commits: [
+      {
+        id: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+        short_id: "6dcb09b5",
+        created_at: "2023-06-15T10:30:00Z",
+        parent_ids: [
+          "1a0b36b3cdad1d2ee32457c102a8c0b7056fa863"
+        ],
+        title: "Fix memory leak in data processing module",
+        message: "Fix memory leak in data processing module\n\nThis commit addresses the memory leak issue reported in #15.",
+        author_name: "John Doe",
+        author_email: "john@example.com",
+        authored_date: "2023-06-15T10:25:00Z",
+        committer_name: "John Doe",
+        committer_email: "john@example.com",
+        committed_date: "2023-06-15T10:30:00Z",
+        web_url: "https://gitlab.com/namespace/awesome-project/-/commit/6dcb09b5b57875f334f61aebed695e2e4193db5e"
+      }
+    ],
+    pipelines: [
+      {
+        id: 1,
+        project_id: 1,
+        status: "success",
+        ref: "main",
+        sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+        web_url: "https://gitlab.com/namespace/awesome-project/-/pipelines/1",
+        created_at: "2023-06-15T10:35:00Z",
+        updated_at: "2023-06-15T10:40:00Z",
+        started_at: "2023-06-15T10:35:05Z",
+        finished_at: "2023-06-15T10:39:55Z",
+        duration: 290,
+        coverage: "87.5"
+      }
+    ]
+  };
